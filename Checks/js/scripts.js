3a15e0b751ba186e61fd7a387eb519d294d62282
Martin Sakwa <sakwamatsotso@gmail.com>
2022-01-20 08:09:33 +0300

    updated styles.css and jQuery

---
+++ Checks/js/scripts.js
@@ -0,0 +1,10 @@
+jQuery('h3').click(function(){
+    alert("Here we do design, thank you") 
+  });
+  jQuery('h4').click(function(){
+    alert("Here we do development, thank you") 
+  });
+
+  jQuery('h5').click(function(){
+    alert("Here we do product management, thank you") 
+  });
