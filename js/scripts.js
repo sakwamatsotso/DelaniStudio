e0d69070306cc051326fa31ed54784dee15a4c6c
Martin Sakwa <sakwamatsotso@gmail.com>
2022-01-21 09:29:23 +0300

    styling the button with pop up alerts

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,7 @@ jQuery('h4').click(function(){
 jQuery('h5').click(function(){
   alert("Here we do product management, thank you") 
 });
-
+//styling the button with pop up alerts
+jQuery('h6').click(function(){
+  alert(" Hello!!.. Thank you for reaching out to us. We have received your message and we shall be contacting you through your email ") 
+});
