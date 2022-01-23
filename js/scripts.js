f52f90dd2759cdb79be6397d9365d1b52961e48d
Martin Sakwa <sakwamatsotso@gmail.com>
2022-01-23 15:41:37 +0300

    created hover effect for portfolio images 2,3 4

--- js/scripts.js
+++ js/scripts.js
@@ -80,9 +80,73 @@ $(document).ready(function() {
 
 $(document).ready(function(){
 $("#image1").mouseover(function(){
-  $(".overlay4").show({opacity: '1'});
+  $(".overlay1").show({opacity: '1'});
   
 }).mouseout(function(){
-  $(".overlay4").hide({opacity: '0'});
+  $(".overlay1").hide({opacity: '0'});
 });
 });
+
+$(document).ready(function(){
+  $("#image2").mouseover(function(){
+    $(".overlay2").show({opacity: '1'});
+    
+  }).mouseout(function(){
+    $(".overlay2").hide({opacity: '0'});
+  });
+  });
+
+$(document).ready(function(){
+  $("#image3").mouseover(function(){
+    $(".overlay3").show({opacity: '1'});
+    
+  }).mouseout(function(){
+    $(".overlay3").hide({opacity: '0'});
+  });
+  });
+  
+  $(document).ready(function(){
+    $("#image4").mouseover(function(){
+      $(".overlay4").show({opacity: '1'});
+      
+    }).mouseout(function(){
+      $(".overlay4").hide({opacity: '0'});
+    });
+    });
+
+    
+$(document).ready(function(){
+  $("#image5").mouseover(function(){
+    $(".overlay5").show({opacity: '1'});
+    
+  }).mouseout(function(){
+    $(".overlay5").hide({opacity: '0'});
+  });
+  });
+  
+$(document).ready(function(){
+  $("#image6").mouseover(function(){
+    $(".overlay6").show({opacity: '1'});
+    
+  }).mouseout(function(){
+    $(".overlay6").hide({opacity: '0'});
+  });
+  });
+  
+  $(document).ready(function(){
+    $("#image7").mouseover(function(){
+      $(".overlay7").show({opacity: '1'});
+      
+    }).mouseout(function(){
+      $(".overlay7").hide({opacity: '0'});
+    });
+    });
+  
+  $(document).ready(function(){
+    $("#image8").mouseover(function(){
+      $(".overlay8").show({opacity: '1'});
+      
+    }).mouseout(function(){
+      $(".overlay8").hide({opacity: '0'});
+    });
+    });
